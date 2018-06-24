7fd8d0309892e71f848faab317c6c49231ad8b21
PeipeiQ <597647185@qq.com>
2018-06-24 20:51:12 +0800

    添加库

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+target "SDKLearning" do
+pod 'AFNetworking'
+pod 'MJExtension'
+pod 'SDWebImage'
+pod 'Masonry'
+end
--- SDKLearning/ViewController.m
+++ SDKLearning/ViewController.m
@@ -6,8 +6,26 @@
 //  Copyright © 2018年 沛沛. All rights reserved.
 //
 
-#import "ViewController.h"
 
+
+#import "ViewController.h"
+#import "Masonry.h"
+typedef NSNumber* (^addBlcok)(int);
+
+//nsnumber扩展
+@interface NSNumber (num)
+-(addBlcok)add;
+@end
+
+@implementation NSNumber (num)
+-(addBlcok)add{
+    addBlcok addblock = ^(int a){
+        return @([self intValue]+a);
+    };
+    return addblock;
+}
+
+@end
 @interface ViewController ()
 
 @end
@@ -17,15 +35,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    UIView *pview = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
+    pview.backgroundColor = [UIColor redColor];
+    pview.center = self.view.center;
+    [self.view addSubview:pview];
     
+    UIView *tview = [[UIView alloc]init];
+    tview.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:tview];
+    [tview mas_makeConstraints:^(MASConstraintMaker *make) {
+        //先完整走完size方法，返回一个constraint实例
+        make.size.equalTo(pview);
+        make.center.equalTo(pview).with.offset(-100);
+        
+    }];
+    MASConstraint *m = [[MASConstraint alloc]init];
+    //链式语法
+    NSNumber *num = @(20);
+    NSNumber *res = num.add(120);
+//    NSLog(@"%@" ,num.add(120).add(150));
     
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
+
+
+
+
